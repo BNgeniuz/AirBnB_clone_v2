2e5e60278fb65f5fc759045752a4a81a89112616
Godwin Bruce <godwin.bruce@gmail.com>
2024-05-16 13:36:36 -0700

    user update

--- models/user.py
+++ models/user.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+'''This module updates user and inherits from BaseModel and Base'''
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    '''This class represents the user by various attributes'''
     __tablename__ = 'users'
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-
+    if storage_type == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-
-        places = relationship("Place", backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete-orphan")
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', cascade="all,delete", backref="user")
+        reviews = relationship('Review', cascade="all,delete", backref="user")
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = "")
