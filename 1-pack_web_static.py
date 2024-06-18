93787d5e7bca3d2ceeec1d2abfad01cc794ebbb5
Godwin Bruce <godwin.bruce@gmail.com>
2024-06-17 21:52:45 -0700

    commit all

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""document models"""
+"""Generate .tgz file from the contents of the web_static folder"""
 
-from fabric.api import task, local
+from fabric import local
 from datetime import datetime
+from os.path import isdir
 
-
-@task
 def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+
+
